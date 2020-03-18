a0d08c63a7098f80ac4f1f2628f1788eeacba00d
vanesa-tamu <vahe5495@tamu.edu>
2020-03-18 10:29:39 -0500

    filter and map array of objects example

---
+++ functions/arrowF.js
@@ -0,0 +1,101 @@
+const videoData = [
+    {
+        name: 'Miss Scarlet',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Mrs. White',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Reverend Green',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Rusty',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Colonel Mustard',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Professor Plum',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    }
+];
+
+// console.log(videoData[0])
+
+//filter and map for this output: ['Miss Scarlet', ...]
+
+const allNames = videoData.filter((char) => {
+    return char.present === true
+     
+})
+
+
+const present = allNames.map(c => {
+    return c.name
+})
+
+console.log(present) 
+
+/*
+
+[
+    'Miss Scarlet',
+    'Reverend Green',
+    'Colonel Mustard',
+    'Professor Plum'
+]
+
+*/
